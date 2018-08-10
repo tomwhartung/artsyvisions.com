e1d779914f24b42bb774ae2b852fbb89519e7dea
Tom W Hartung <tomwhartung@gmail.com>
2018-08-10 14:16:54 -0600

    Adding run.sh , a shell script to run the development server.

---
+++ Site/bin/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# run.sh: run the development server for this app
+# -----------------------------------------------
+#
+# Setting RUNNING_LOCALLY does the following:
+# - causes site to display grey boxes instead of ads
+# - enables the tiny sized quiz (containing 4 questions for testing) option
+# - turns on the django DEBUG option (in settings.py)
+#   (this is NOT the same as DJANGO_DEBUG)
+#
+# Setting DJANGO_DEBUG does the following:
+# - prints detailed information about how quizzes are scored
+#
+export DJANGO_DEBUG=1
+export RUNNING_LOCALLY=1
+export PYTHONPATH="..:${PYTHONPATH}"
+cd ..
+. /var/www/artsyvisions.com/htdocs/artsyvisions.com/virtualenvs/artsyvisions_env/bin/activate
+python manage.py runserver 0.0.0.0:8001
