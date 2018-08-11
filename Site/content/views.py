66028fce511e3b5c1dcf05968de1a452435af485
Tom W Hartung <tomwhartung@gmail.com>
2018-08-11 17:59:43 -0600

    Added view methods for all four legal documents to views.py .

--- Site/content/views.py
+++ Site/content/views.py
@@ -24,3 +24,57 @@ def home(request):
         'title': title,
     }
     return render(request, template, context)
+
+##
+## -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+##   Views for Legal Pages
+## -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+##
+
+
+def affiliate_marketing_disclosure(request):
+
+    """ Load and render the affiliate_marketing_disclosure template """
+
+    title = 'Disclosure - ArtsyVisions.com';
+    template = 'content/legal/affiliate_marketing_disclosure.html'
+    context = {
+        'title': title,
+    }
+    return render(request, template, context)
+
+
+def privacy_policy(request):
+
+    """ Load and render the privacy_policy template """
+
+    title = 'Privacy Policy - ArtsyVisions.com';
+    template = 'content/legal/privacy_policy.html'
+    context = {
+        'title': title,
+    }
+    return render(request, template, context)
+
+
+def questionnaire_disclaimer(request):
+
+    """ Load and render the questionnaire_disclaimer template """
+
+    title = 'Disclaimer - ArtsyVisions.com';
+    template = 'content/legal/questionnaire_disclaimer.html'
+    context = {
+        'title': title,
+    }
+    return render(request, template, context)
+
+
+def terms_of_service(request):
+
+    """ Load and render the terms_of_service template """
+
+    title = 'Terms of Service - ArtsyVisions.com';
+    template = 'content/legal/terms_of_service.html'
+    context = {
+        'title': title,
+    }
+    return render(request, template, context)
