1074997fd354b6a033a15e91ec859d679251a4e3
Tom W Hartung <tomwhartung@gmail.com>
2018-08-11 11:45:42 -0600

    Updated views.py with an index method that displays a "Hello world" message.

--- Site/content/views.py
+++ Site/content/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+##
+## from django.shortcuts import render
+##
+## # Create your views here.
 
-# Create your views here.
+#
+# From "Write your first view" in:
+# https://docs.djangoproject.com/en/2.1/intro/tutorial01/#write-your-first-view
+#
+from django.http import HttpResponse
+
+
+def index(request):
+        return HttpResponse("Hello, world. You're at the content index.")
+
