a28d75f3722e6cad40204075a7d4882507b6bebb
Tom W Hartung <tomwhartung@gmail.com>
2018-08-11 15:20:01 -0600

    Added paths for our home page to urls.py .

--- Site/content/urls.py
+++ Site/content/urls.py
@@ -13,6 +13,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('home', views.home, name='home'),
+
 ]
 
