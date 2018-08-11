73fbdb715300a294709430bce3008804f482ee28
Tom W Hartung <tomwhartung@gmail.com>
2018-08-11 15:20:49 -0600

    Totally updated views.py to work with our new home page.  Sweet.

--- Site/content/views.py
+++ Site/content/views.py
@@ -1,15 +1,26 @@
-##
-## from django.shortcuts import render
-##
-## # Create your views here.
+""" views.py for our content app
 
-#
-# From "Write your first view" in:
-# https://docs.djangoproject.com/en/2.1/intro/tutorial01/#write-your-first-view
-#
+Purpose: define the views for this app
+Author: Tom W. Hartung
+Date: Summer, 2018.
+Copyright: (c) 2018 Tom W. Hartung, Groja.com, and JooMoo Websites LLC.
+Reference:
+  (none)
+"""
+
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+from django.views.generic.base import View
 
 
-def index(request):
-        return HttpResponse("Hello, world. You're at the content index.")
+def home(request):
 
+    """ Load and render the Home page template """
+
+    title = 'Home - ArtsyVisions.com';
+    template = 'content/home.html'
+    context = {
+        'title': title,
+    }
+    return render(request, template, context)
