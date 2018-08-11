0d16c768cd1deefa8b178684ba2f8666d3c9e092
Tom W Hartung <tomwhartung@gmail.com>
2018-08-11 11:48:52 -0600

    Updated content/urls.py , adding a route, so that visiting the site invokes `views.index` , the `index()` function in `views.py` .

---
+++ Site/content/urls.py
@@ -0,0 +1,18 @@
+""" urls.py for our content app
+
+Purpose: define the urls for this app
+Author: Tom W. Hartung
+Date: Summer, 2018.
+Copyright: (c) 2018 Tom W. Hartung, Groja.com, and JooMoo Websites LLC.
+Reference:
+    https://docs.djangoproject.com/en/2.1/intro/tutorial01/#write-your-first-view
+"""
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
+
