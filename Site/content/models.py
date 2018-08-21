c2a0c95f99dfde7597073476cebddd7a6a4a24d6
Tom W Hartung <tomwhartung@gmail.com>
2018-08-20 19:28:28 -0600

    Added header comments copied from the models.py file in seeourminds.com to models.py .

--- Site/content/models.py
+++ Site/content/models.py
@@ -1,3 +1,13 @@
+""" Contains the non-database models for our app.
+
+Purpose: contains the models for unsaved data and read-only data in json format
+Author: Tom W. Hartung
+Date: Summer, 2018.
+Copyright: (c) 2018 Tom W. Hartung, Groja.com, and JooMoo Websites LLC.
+Reference:
+      (none, yet)
+"""
+
 from django.db import models
 
 # Create your models here.
