4664a83b54ff6f34cb7ce4fa1b28d6631ca56088
Tom W Hartung <tomwhartung@gmail.com>
2018-11-23 14:47:00 -0700

    Updated Site/Site/urls.py to use the url() method instead of path() , because path() is available only in version 2.0+.

--- Site/Site/urls.py
+++ Site/Site/urls.py
@@ -14,9 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import *
+
+"""
+When upgrading to django 2.0+, we can use the path method:
+    https://docs.djangoproject.com/en/2.0/ref/urls/#path
+
 from django.urls import include, path
 
+The routes we are using are quite simple, and we already have a
+version of this file that uses the path method:
+    ./urls-uses_path-save_for_2.0.py
+More:
+    https://stackoverflow.com/questions/47947673/is-it-better-to-use-path-or-url-in-urls-py-for-django-2-0
+"""
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('content.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('content.urls')),
 ]
