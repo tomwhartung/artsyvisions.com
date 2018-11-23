7507731e177115c6457191819cf3b40dade7b803
Tom W Hartung <tomwhartung@gmail.com>
2018-11-23 14:47:54 -0700

    Updated Site/content/urls.py to use the url() method instead of path() , because path() is available only in version 2.0+.

--- Site/content/urls.py
+++ Site/content/urls.py
@@ -8,46 +8,59 @@ Reference:
     https://docs.djangoproject.com/en/2.1/intro/tutorial01/#write-your-first-view
 """
 
-from django.urls import path
+from django.conf.urls import *
 
 from . import views
 
+"""
+When upgrading to django 2.0+, we can use the path method:
+    https://docs.djangoproject.com/en/2.0/ref/urls/#path
+
+from django.urls import path
+
+The routes we are using are quite simple, and we already have a
+version of this file that uses the path method:
+    ./urls-uses_path-save_for_2.0.py
+More:
+    https://stackoverflow.com/questions/47947673/is-it-better-to-use-path-or-url-in-urls-py-for-django-2-0
+"""
+
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('home', views.home, name='home'),
-    path('index', views.index, name='index'),
-    path('v', views.versions, name='versions'),
-    path('visions/all',
+    url(r'^$', views.home, name='home'),
+    url(r'^home$', views.home, name='home'),
+    url(r'^index$', views.index, name='index'),
+    url(r'^v$', views.versions, name='versions'),
+    url(r'^visions/all$',
         views.visions_all,
         name='visions_all'),
-    path('visions/person',
+    url(r'^visions/person$',
         views.visions_person,
         name='visions_person'),
-    path('visions/pairs',
+    url(r'^visions/pairs$',
         views.visions_pairs,
         name='visions_pairs'),
-    path('visions/groups',
+    url(r'^visions/groups$',
         views.visions_groups,
         name='visions_groups'),
-    path('visions/story/',
+    url(r'^visions/story/$',
         views.visions_story,
         name='visions_story'),
-    path('visions/story/<vision_file_no_ext>',
+    url(r'^visions/story/(?P<vision_file_no_ext>\S+)$',
         views.visions_story,
         name='visions_story'),
-    path('legal/affiliate_marketing_disclosure',
+    url(r'^legal/affiliate_marketing_disclosure$',
         views.affiliate_marketing_disclosure,
         name='affiliate_marketing_disclosure'),
-    path('legal/privacy_policy',
+    url(r'^legal/privacy_policy$',
         views.privacy_policy,
         name='privacy_policy'),
-    path('legal/questionnaire_disclaimer',
+    url(r'^legal/questionnaire_disclaimer$',
         views.questionnaire_disclaimer,
         name='questionnaire_disclaimer'),
-    path('legal/terms_of_service',
+    url(r'^legal/terms_of_service$',
         views.terms_of_service,
         name='terms_of_service'),
-    path('<path:unknown_page>',
+    url(r'^<path:unknown_page>$',
         views.not_found,
         name='not_found'),
 
