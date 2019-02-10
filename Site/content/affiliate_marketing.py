ad9a472226478bb56826ecef6aa951a7dc733eeb
Tom W Hartung <tomwhartung@gmail.com>
2019-02-10 14:16:58 -0700

    Updated affiliate_marketing.py with a new dictionary, afl_default, containing links to the new afl_default conversion page on groja.com, and used those values in the afl_content and afl_button dictionaries.

--- Site/content/affiliate_marketing.py
+++ Site/content/affiliate_marketing.py
@@ -18,10 +18,27 @@ class AffiliateLinks:
     #
     # Source Link Dictionaries:
     #
+    afl_default = {
+        'ah_by_chernow': 'https://groja.com/conversion/afl_default',
+        'ah_ax_video': 'https://groja.com/conversion/afl_default',
+        'concerning_by_kandinsky': 'https://groja.com/conversion/afl_default',
+        'fawlty_towers': 'https://groja.com/conversion/afl_default',
+        'game_of_thrones': 'https://groja.com/conversion/afl_default',
+        'star_trek_tos': 'https://groja.com/conversion/afl_default',
+        'tj_himself': 'https://groja.com/conversion/afl_default',
+        'tj_by_burns': 'https://groja.com/conversion/afl_default',
+        'twin_peaks': 'https://groja.com/conversion/afl_default',
+        'weirdsville_usa': 'https://groja.com/conversion/afl_default',
+        'wild_at_heart': 'https://groja.com/conversion/afl_default',
+        'wild_heart_book': 'https://groja.com/conversion/afl_default',
+        'x_files': 'https://groja.com/conversion/afl_default',
+        'xxx': 'https://groja.com/conversion/afl_default',
+    }
+
     afl_none = {
-        'ah_chernow': '',
+        'ah_by_chernow': '',
         'ah_ax_video': '',
-        'concerning': '',
+        'concerning_by_kandinsky': '',
         'fawlty_towers': '',
         'game_of_thrones': '',
         'star_trek_tos': '',
@@ -47,39 +64,40 @@ class AffiliateLinks:
         Assign source links to active links
         """
 
-        self.afl_content['ah_chernow'] = afl_none['ah_chernow']
-        self.afl_button['ah_chernow'] = afl_none['ah_chernow']
-        self.afl_content['ah_ax_video'] = afl_none['ah_ax_video']
-        self.afl_button['ah_ax_video'] = afl_none['ah_ax_video']
+        self.afl_content['ah_by_chernow'] = self.afl_default['ah_by_chernow']
+        self.afl_button['ah_by_chernow'] = self.afl_default['ah_by_chernow']
+        self.afl_content['ah_ax_video'] = self.afl_default['ah_ax_video']
+        self.afl_button['ah_ax_video'] = self.afl_default['ah_ax_video']
 
-        self.afl_content['concerning'] = afl_none['concerning']
+        self.afl_content['concerning_by_kandinsky'] \
+            = self.afl_default['concerning_by_kandinsky']
 
-        self.afl_content['fawlty_towers'] = afl_none['fawlty_towers']
-        self.afl_button['fawlty_towers'] = afl_none['fawlty_towers']
+        self.afl_content['fawlty_towers'] = self.afl_default['fawlty_towers']
+        self.afl_button['fawlty_towers'] = self.afl_default['fawlty_towers']
 
-        self.afl_content['game_of_thrones'] = afl_none['game_of_thrones']
-        self.afl_button['game_of_thrones'] = afl_none['game_of_thrones']
+        self.afl_content['game_of_thrones'] = self.afl_default['game_of_thrones']
+        self.afl_button['game_of_thrones'] = self.afl_default['game_of_thrones']
 
-        self.afl_content['star_trek_tos'] = afl_none['star_trek_tos']
-        self.afl_button['star_trek_tos'] = afl_none['star_trek_tos']
+        self.afl_content['star_trek_tos'] = self.afl_default['star_trek_tos']
+        self.afl_button['star_trek_tos'] = self.afl_default['star_trek_tos']
 
-        self.afl_content['tj_himself'] = afl_none['tj_himself']
-        self.afl_button['tj_himself'] = afl_none['tj_himself']
-        self.afl_content['tj_by_burns'] = afl_none['tj_by_burns']
-        self.afl_button['tj_by_burns'] = afl_none['tj_by_burns']
+        self.afl_content['tj_himself'] = self.afl_default['tj_himself']
+        self.afl_button['tj_himself'] = self.afl_default['tj_himself']
+        self.afl_content['tj_by_burns'] = self.afl_default['tj_by_burns']
+        self.afl_button['tj_by_burns'] = self.afl_default['tj_by_burns']
 
-        self.afl_content['x_files'] = afl_none['x_files']
-        self.afl_button['x_files'] = afl_none['x_files']
+        self.afl_content['x_files'] = self.afl_default['x_files']
+        self.afl_button['x_files'] = self.afl_default['x_files']
 
 
-        self.afl_content['twin_peaks'] = afl_none['twin_peaks']
-        self.afl_button['twin_peaks'] = afl_none['twin_peaks']
-        self.afl_content['wild_at_heart'] = afl_none['wild_at_heart']
-        self.afl_button['wild_at_heart'] = afl_none['wild_at_heart']
-        self.afl_content['wild_heart_book'] = afl_none['wild_heart_book']
-        self.afl_button['wild_heart_book'] = afl_none['wild_heart_book']
-        self.afl_content['weirdsville_usa'] = afl_none['weirdsville_usa']
-        self.afl_button['weirdsville_usa'] = afl_none['weirdsville_usa']
+        self.afl_content['twin_peaks'] = self.afl_default['twin_peaks']
+        self.afl_button['twin_peaks'] = self.afl_default['twin_peaks']
+        self.afl_content['wild_at_heart'] = self.afl_default['wild_at_heart']
+        self.afl_button['wild_at_heart'] = self.afl_default['wild_at_heart']
+        self.afl_content['wild_heart_book'] = self.afl_default['wild_heart_book']
+        self.afl_button['wild_heart_book'] = self.afl_default['wild_heart_book']
+        self.afl_content['weirdsville_usa'] = self.afl_default['weirdsville_usa']
+        self.afl_button['weirdsville_usa'] = self.afl_default['weirdsville_usa']
 
-        self.afl_content['xxx'] = afl_none['xxx']
-        self.afl_button['xxx'] = afl_none['xxx']
+        self.afl_content['xxx'] = self.afl_default['xxx']
+        self.afl_button['xxx'] = self.afl_default['xxx']
